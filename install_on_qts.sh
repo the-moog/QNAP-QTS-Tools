1d1cf9064441cde37431a35cb970d0f623eba6bf
the-moog <3725802+the-moog@users.noreply.github.com>
2022-05-28 07:43:37 +0100

    Replace multiple install scripts with a single script that also calls builds

--- install_on_qts.sh
+++ install_on_qts.sh
@@ -6,30 +6,31 @@ fi
 
 BASE_DIR=$(dirname $0)
 BUILD_SCRIPT_NAME="build-$1.sh"
-BUILD_SCRIPT="${BASE_DIR}/${BUILD_SCRIPT_NAME}"
+BUILD_SCRIPT="${BASE_DIR}/scripts/${BUILD_SCRIPT_NAME}"
 CONTAINER="moog/builder"
+DEST="/share/Public"
 
 # Dev tools: gcc etc
-if [ ! -d /share/Public/toolchain ]; then
-    mkdir /share/Public/toolchain
+if [ ! -d ${DEST}/toolchain ]; then
+    mkdir ${DEST}/toolchain
 fi
 
 # Non dev tools
-if [ ! -d /share/Public/tools ]; then
-    mkdir /share/Public/tools
+if [ ! -d ${DEST}/tools ]; then
+    mkdir ${DEST}/tools
 fi
 
 
 if ! [ -f ${BUILD_SCRIPT} ]; then
-    echo "Expected script ${BUILD_SCRIPT} required to build $1 is missing"
+    echo "The script ${BUILD_SCRIPT} that is required to build $1 is missing"
     exit 1
 fi
 
 
 if [ -z "$(system-docker ps | grep 'builder')" ]; then
     system-docker run --name builder --privileged \
-    -v /share/Public/toolchain:/share/Public/toolchain \
-    -v /share/Public/tools:/share/Public/tools \
+    -v ${DEST}/toolchain:${DEST}/toolchain \
+    -v ${DEST}/tools:${DEST}/tools \
     -d ${CONTAINER}
 fi
 
@@ -37,20 +38,23 @@ system-docker cp ${BUILD_SCRIPT} builder:/root/${BUILD_SCRIPT_NAME}
 system-docker exec -t builder bash /root/${BUILD_SCRIPT_NAME}
 
 if [ -z "$(cat /etc/profile | grep 'Public/tools')" ]; then
-    echo "\n\nPlease run"
-    echo "   echo PATH=/share/Public/tools/bin:\$PATH >> /etc/profile"
-    echo "as root"
+    echo -e "\n\nPlease run"
+    echo -e "   echo PATH=${DEST}/tools/bin:\$PATH >> /etc/profile"
+    echo -e "as root"
 fi
 
 if [ -z "$(cat /etc/ld.so.conf | grep 'Public/tools')" ]; then
-    echo "\n\nPlease run"
-    echo "   echo /share/Public/tools/lib >> /etc/ld.so.conf"
-    echo "as root"
+    echo -e "\n\nPlease run"
+    echo -e "   echo ${DEST}/tools/lib >> /etc/ld.so.conf"
+    echo -e "as root"
 fi
 
-echo "Remember to run"
-echo "   ldconfig -v -n /share/Public/tools/lib"
-echo "as root"
+echo -e "\n\nRemember to run"
+echo -e "   ldconfig -v -n ${DEST}/tools/lib && ldconfig"
+echo -e "as root"
+
+echo -e "\n\n\n"
 
 system-docker stop builder; system-docker rm builder
 
+echo -e "\n$0 DONE\n\n"
